57a8cf2ab3c6dabfdf13a05d6b979036b85cfc1e
Stephen Caudill <scaudill@gmail.com>
2017-10-19 17:00:54 -0600

    Very minimal git setup

--- bork/vim.sh
+++ bork/vim.sh
@@ -6,7 +6,7 @@ destination $HOME/.vim/bundle
 # vim bundles
 
 ## colors
-ok github voxdolo/Muono
+ok github voxdolo/muono
 
 ## misc
 ok github tpope/vim-pathogen
@@ -16,10 +16,6 @@ ok github tpope/vim-markdown
 ok github tpope/vim-sensible
 ok github tpope/vim-unimpaired
 ok github scrooloose/nerdtree
-
-## UI
-ok github itchyny/lightline.vim
-ok github kien/rainbow_parentheses.vim
 
 ## Buffers
 ok github vim-scripts/bufkill.vim
@@ -34,32 +30,3 @@ ok github tpope/vim-repeat
 ok github tpope/vim-rsi
 ok github tpope/vim-sleuth
 ok github tpope/vim-surround
-
-## Navigation
-ok github tpope/vim-projectionist
-
-## git
-ok github tpope/vim-git
-ok github tpope/vim-fugitive
-
-## Ruby
-ok github vim-ruby/vim-ruby
-ok github tpope/vim-bundler
-ok github tpope/vim-cucumber
-ok github tpope/vim-haml
-ok github tpope/vim-rails
-ok github tpope/vim-rake
-ok github jgdavey/vim-turbux
-
-## clojure
-ok github tpope/vim-classpath
-ok github guns/vim-clojure-static
-ok github tpope/vim-fireplace
-ok github tpope/vim-leiningen
-ok github guns/vim-sexp
-ok github tpope/vim-sexp-mappings-for-regular-people
-
-## javascript
-ok github kchmck/vim-coffee-script
-ok github pangloss/vim-javascript
-ok github leshill/vim-json
